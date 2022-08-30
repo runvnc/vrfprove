86661e063afda10f3851b050b02e8ff64569a802
Jason Livesay <runvnc@gmail.com>
2022-08-29 23:57:17 -0500

    update

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-//	"crypto/sha512"
+	"crypto/sha512"
 	"encoding/binary"
 	"os"
   "math/rand"
 	//"strconv"
 	//"strings"
 	"fmt"
-  "reflect"
 
+	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/crypto"
 	//"github.com/algorand/go-algorand-sdk/abi"
 	//"github.com/algorand/go-algorand-sdk/client/v2/algod"
@@ -18,31 +18,27 @@ import (
 //	"github.com/algorand/go-algorand-sdk/types"
 )
 
-func main() {
-//	var pubKeyStr = os.Getenv("VRFPUB")
-//	var pubKey, _ = types.DecodeAddress(pubKeyStr)
-	var vrfMnemonic = os.Getenv("VRFPRIV")
+func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
+	pk, err := mnemonic.ToPrivateKey(phrase)
 
-	privKey, err := mnemonic.ToPrivateKey(vrfMnemonic)
-
-	privKey_ := [64]byte{}
-	copy(privKey_[:], privKey[:])
-
-	//alpha = make([]byte, hex.DecodedLen(len(alphaHex)))
-	//mustDecode(t, alpha, alphaHex)
-
-	//pkTest, sk := VrfKeygenFromSeed(seed)
-
-	//piTest, ok := sk.proveBytes(alpha)
-	
-  var vrfPrivateKeyx crypto.VrfPrivkey
-  copy(vrfPrivateKeyx[:], privKey_[:])
-
-	if err != nil {
-		fmt.Printf("invalid vrf mnemonic: %v", err)
-		return
+  if err != nil {
+		panic("invalid vrf mnemonic: %v")
 	}
 
+  var vrfPK crypto.VrfPrivkey
+  copy(vrfPK[:], pk[:])
+  return vrfPK
+}
+
+type Msg []byte
+
+func (m Msg) ToBeHashed() (protocol.HashID, []byte) {
+	return protocol.HashID(""), m[:]
+}
+
+func main() {
+	sk := toVrfPrivKey(os.Getenv("VRFPRIV"))
+	
 	var block_seed [32]byte
   for i := 0; i < 32; i++ {
     block_seed[i] = byte(rand.Intn(255))
@@ -53,18 +49,13 @@ func main() {
 	var buff = make([]byte, 32+8)
 	binary.BigEndian.PutUint64(buff, uint64(round))
 	copy(buff[8:], block_seed[:])
-	//var vrfInput = sha512.Sum512_256(buff[:])
+	var vrfInput = sha512.Sum512_256(buff[:])
 
-  fooType := reflect.TypeOf(vrfPrivateKeyx)
-  for i := 0; i < fooType.NumMethod(); i++ {
-    method := fooType.Method(i)
-    fmt.Println(method.Name)
-  }
-
-	vrfProof, _ := vrfPrivateKeyx.Prove(vrfInput[:])
+	vrfProof, _ := sk.Prove(Msg(vrfInput[:]))
 	vrfHash, _ := vrfProof.Hash()
      
-	fmt.Print(vrfProof)
+	fmt.Println(vrfProof)
+  fmt.Println("--------------------")
 	fmt.Print(vrfHash)
 }
 
