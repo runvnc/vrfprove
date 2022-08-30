d9c2600d14c1131429bb61b1d72f2a223f9b309b
Jason Livesay <runvnc@gmail.com>
2022-08-30 18:44:55 -0500

    update

--- main.go
+++ main.go
@@ -4,19 +4,12 @@ import (
 	"crypto/sha512"
 	"encoding/binary"
 	"os"
-  "math/rand"
-  "bufio"
-	//"strconv"
-	//"strings"
+  "encoding/base32"
+  "strconv"
 	"fmt"
-
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/crypto"
-	//"github.com/algorand/go-algorand-sdk/abi"
-	//"github.com/algorand/go-algorand-sdk/client/v2/algod"
 	"github.com/algorand/go-algorand-sdk/mnemonic"
-	//"github.com/algorand/go-algorand-sdk/crypto"
-//	"github.com/algorand/go-algorand-sdk/types"
 )
 
 func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
@@ -32,17 +25,17 @@ func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
 type Msg []byte
 
 func (m Msg) ToBeHashed() (protocol.HashID, []byte) {
-	return protocol.HashID(""), sha512.Sum512_256(m[:])
+	id, sum := protocol.HashID(""), sha512.Sum512_256(m[:])
+  return id, sum[:]
 }
 
 func main() {
 	sk := toVrfPrivKey(os.Getenv("VRFPRIV"))
-	var stdin = bufio.NewReader(os.Stdin)
-  round := strconv.Atoi(stdin.ReadString(' '))  
-  seedStr = stdin.ReadString(' ')
+  round, err := strconv.Atoi(os.Args[1])
+  seedStr := os.Args[2]
 
 	var block_seed [32]byte
-  _, err := base32.StdEncoding.Decode(block_seed, []byte(seedStr))
+  _, err = base32.StdEncoding.Decode(block_seed[:], []byte(seedStr))
   if err != nil { panic("Invalid block seed") }
 
 	var buff = make([]byte, 32+8)
@@ -52,27 +45,7 @@ func main() {
 	vrfProof, _ := sk.Prove(Msg(buff[:]))
 	vrfHash, _ := vrfProof.Hash()
   
-  fmt.Println("Block seed:",block_seed,"round:",round)
-  fmt.Println("Proof:")
-	fmt.Println(vrfProof)
-  fmt.Println("Hash:")
-	fmt.Print(vrfHash)
+	fmt.Print(base32.StdEncoding.EncodeToString(vrfProof[:]))
+	fmt.Println(base32.StdEncoding.EncodeToString(vrfHash[:]))
 }
-
-/*
-
-	var block_seed = cert.Block.Seed()
-	var round = cert.Block.Round()
-
-	buff := make([]byte, 32+8)
-	binary.BigEndian.PutUint64(buff, uint64(round))
-	copy(buff[8:], block_seed[:])
-	vrfInput := sha512.Sum512_256(buff[:])
-
-	hash, proof := vrfp.Prove(vrfInput[:])
-     
-	fmt.Print(hash)
-	fmt.Print(proof)
-}
-*/
 
