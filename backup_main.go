3a69eedafbab011385a87bbf82e3a0be5cfb67a7
Jason Livesay <runvnc@gmail.com>
2022-08-30 15:10:11 -0500

    update

--- backup_main.go
+++ backup_main.go
@@ -22,7 +22,9 @@ import (
 func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
 	pk, err := mnemonic.ToPrivateKey(phrase)
 
-  if err != nil { panic("invalid vrf mnemonic: %v") }
+  if err != nil {
+		panic("invalid vrf mnemonic: %v")
+	}
 
   var vrfPK crypto.VrfPrivkey
   copy(vrfPK[:], pk[:])
@@ -31,31 +33,37 @@ func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
 
 type Msg []byte
 
+//return protocol.HashID(""), m[:]
+
 func (m Msg) ToBeHashed() (protocol.HashID, []byte) {
-	return protocol.HashID(""), sha512.Sum512_256(m[:])
+  var vrfInput = sha512.Sum512_256(m)
+
+
+	return protocol.HashID(""), m[:]
 }
 
 func main() {
 	sk := toVrfPrivKey(os.Getenv("VRFPRIV"))
 	var stdin = bufio.NewReader(os.Stdin)
-  round := strconv.Atoi(stdin.ReadString(' '))  
-  seedStr = stdin.ReadString(' ')
+
 
 	var block_seed [32]byte
-  _, err := base32.StdEncoding.Decode(block_seed, []byte(seedStr))
-  if err != nil { panic("Invalid block seed") }
+  for i := 0; i < 32; i++ {
+    block_seed[i] = byte(rand.Intn(255))
+  }
+
+  var round = 834834520
 
 	var buff = make([]byte, 32+8)
 	binary.BigEndian.PutUint64(buff, uint64(round))
 	copy(buff[8:], block_seed[:])
+	var vrfInput = sha512.Sum512_256(buff[:])
 
-	vrfProof, _ := sk.Prove(Msg(buff[:]))
+	vrfProof, _ := sk.Prove(Msg(vrfInput[:]))
 	vrfHash, _ := vrfProof.Hash()
-  
-  fmt.Println("Block seed:",block_seed,"round:",round)
-  fmt.Println("Proof:")
+     
 	fmt.Println(vrfProof)
-  fmt.Println("Hash:")
+  fmt.Println("--------------------")
 	fmt.Print(vrfHash)
 }
 
