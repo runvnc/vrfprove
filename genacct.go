ef5bdb9abd4c0472fbaecf077aea5290da174935
Jason Livesay <runvnc@gmail.com>
2022-08-30 23:52:04 -0500

    update

--- genacct.go
+++ genacct.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	//"crypto/sha512"
-	//"encoding/binary"
-	//"os"
-	//"strconv"
-	//"strings"
-	"fmt"
-
-	//"github.com/algorand/go-algorand-sdk/abi"
-	//"github.com/algorand/go-algorand-sdk/client/v2/algod"
-	"github.com/algorand/go-algorand-sdk/mnemonic"
-	"github.com/algorand/go-algorand-sdk/crypto"
-	//"github.com/algorand/go-algorand-sdk/types"
+  "fmt"
+  "github.com/algorand/go-algorand-sdk/mnemonic"
+  "github.com/algorand/go-algorand-sdk/crypto"
 )
 
 func main() {
-	var acct = crypto.GenerateAccount()
+  var acct = crypto.GenerateAccount()
   fmt.Println(acct.Address)
   var mnemonic, _ = mnemonic.FromPrivateKey(acct.PrivateKey) 
   fmt.Println(mnemonic)
--- vrfprove.go
+++ vrfprove.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"crypto/sha512"
-	"encoding/binary"
-	"os"
+  "crypto/sha512"
+  "encoding/binary"
+  "os"
   "encoding/base32"
   "strconv"
-	"fmt"
-	"github.com/algorand/go-algorand/protocol"
-	"github.com/algorand/go-algorand/crypto"
-	"github.com/algorand/go-algorand-sdk/mnemonic"
+  "fmt"
+  "github.com/algorand/go-algorand/protocol"
+  "github.com/algorand/go-algorand/crypto"
+  "github.com/algorand/go-algorand-sdk/mnemonic"
 )
 
 func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
-	pk, err := mnemonic.ToPrivateKey(phrase)
+  pk, err := mnemonic.ToPrivateKey(phrase)
 
   if err != nil { panic("invalid vrf mnemonic: %v") }
 
@@ -25,27 +25,27 @@ func toVrfPrivKey(phrase string) (crypto.VrfPrivkey) {
 type Msg []byte
 
 func (m Msg) ToBeHashed() (protocol.HashID, []byte) {
-	id, sum := protocol.HashID(""), sha512.Sum512_256(m[:])
+  id, sum := protocol.HashID(""), sha512.Sum512_256(m[:])
   return id, sum[:]
 }
 
 func main() {
-	sk := toVrfPrivKey(os.Getenv("VRFPRIV"))
+  sk := toVrfPrivKey(os.Getenv("VRFPRIV"))
   round, err := strconv.Atoi(os.Args[1])
   seedStr := os.Args[2]
 
-	var block_seed [32]byte
+  var block_seed [32]byte
   _, err = base32.StdEncoding.Decode(block_seed[:], []byte(seedStr))
   if err != nil { panic("Invalid block seed") }
 
-	var buff = make([]byte, 32+8)
-	binary.BigEndian.PutUint64(buff, uint64(round))
-	copy(buff[8:], block_seed[:])
+  var buff = make([]byte, 32+8)
+  binary.BigEndian.PutUint64(buff, uint64(round))
+  copy(buff[8:], block_seed[:])
 
-	vrfProof, _ := sk.Prove(Msg(buff[:]))
-	vrfHash, _ := vrfProof.Hash()
+  vrfProof, _ := sk.Prove(Msg(buff[:]))
+  vrfHash, _ := vrfProof.Hash()
   
-	fmt.Print(base32.StdEncoding.EncodeToString(vrfProof[:]))
-	fmt.Println(base32.StdEncoding.EncodeToString(vrfHash[:]))
+  fmt.Print(base32.StdEncoding.EncodeToString(vrfProof[:]))
+  fmt.Println(base32.StdEncoding.EncodeToString(vrfHash[:]))
 }
 
